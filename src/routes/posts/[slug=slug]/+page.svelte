b412645ad81c90f88f11d40a6bd3242c55d5ef90
Radu Elisei <radu.elisei@yahoo.com>
2023-08-28 10:54:23 +0300

    Run Posts On Server

--- src/routes/posts/[slug=slug]/+page.svelte
+++ src/routes/posts/[slug=slug]/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
-	import { page } from "$app/stores";
+	import type { PageData } from "./$types";
+
+	export let data: PageData;
+
+	function formatDate(date: Date) {
+		return new Intl.DateTimeFormat("en", { dateStyle: "full" }).format(date);
+	}
 </script>
 
-<h1>Post</h1>
+<hgroup>
+	<h1>{data.post.title}</h1>
+	<h2>{formatDate(data.post.createdAt)}</h2>
+</hgroup>
 
-<pre>
-    {JSON.stringify($page, null, 2)}
-</pre>
+<div class="content">
+	{@html data.post.content}
+</div>
